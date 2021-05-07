e0a06658f62bc48fe7c43a2223f9623a929997b5
Supreethajagan <83803029+Supreethajagan@users.noreply.github.com>
2021-05-07 21:41:31 +0530

    Add files via upload

---
+++ Reg.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Assignment Moolya</title>
+</head>
+<body style="background-color:powderblue;">
+
+<h1>
+Welcome to Moolya,We are excited to share a news that we are hiring people like you.You want to join one of the excellent Organization? Come on,go ahead and fill this form given
+below, We will reach out to you.</h1>
+<h2>Registration Form</h2>
+<form>
+
+
+
+<div>
+<label>FIRST NAME</label>
+
+<input type="text" placeholder="First Name"  required id="fname">
+
+</div>
+
+<div>
+<label>LAST NAME</label>
+<input type="text"   placeholder="Last Name" id="lname">
+</div>
+<div>
+<label>EMAIL ID</label>
+<input type="email"   placeholder="Email"  required id="email">
+</div>
+<div>
+<label>CONTACT NUMBER</label>
+<input type="number_format"  placeholder="Phone"  required id="phone">
+</div>
+
+
+<div>
+<label>GENDER</label>
+<input type="radio" name="GENDER">FEMALE
+<input type="radio" name="GENDER">MALE
+<input type="radio" name="GENDER">OTHER
+</div>
+
+<div>
+
+<!__button type="submit" class=""btn" id="submit">
+<input type="submit" value"SUBMIT">
+</div>
+<table>
+</form>
+</body>
+</html>
